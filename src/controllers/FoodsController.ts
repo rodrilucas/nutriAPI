36540d3c78f0931102272a77ea3c301914e277c3
rodrilucas <lucas.celery@gmail.com>
2025-05-18 10:47:36 -0300

    feat: method to get all the food by pagination

---
+++ src/controllers/FoodsController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import FetchFatSecretAPI from "../services/FetchFatSecretAPI";
+
+class FoodsController {
+  private fetchFatSecretAPI: FetchFatSecretAPI;
+
+  constructor() {
+    this.fetchFatSecretAPI = new FetchFatSecretAPI();
+  }
+
+  getAll = async (req: Request, res: Response) => {
+    const { foodName, page } = req.query;
+    const data = await this.fetchFatSecretAPI.searchFood(
+      foodName as string,
+      page as string
+    );
+    res.json(data);
+  };
+}
+
+export default FoodsController;
