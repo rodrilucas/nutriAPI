86383bb7907104ff29782338fabe62c81a0b67ae
rodrilucas <lucas.celery@gmail.com>
2025-07-15 11:09:00 -0300

    fix: cors origin

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://nutri-lt1ax3u17-rodri-lucas.vercel.app/",
   })
 );
 
